75151b7ee150a1cb30b03e5219192b52ff30bd38
1com <123@nate.com>
2020-05-27 15:51:58 +0900

    random-app 1.1

--- src/random/App.java
+++ src/random/App.java
@@ -1,13 +1,12 @@
-package randomapp;
+package random;
 
 import java.util.Random;
 
-public class RandomApp {
+public class App {
 
 	public static void main(String[] args) {
 		Random r = new Random();
 		int num = r.nextInt(50)+1;
 		System.out.println("random value : " + num);
 	}
-
 }
